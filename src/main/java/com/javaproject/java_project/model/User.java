ef42d1bae4450a4221cf1c3e29bd3839d3747e40
sricharanand <58458878+sricharanand@users.noreply.github.com>
2025-11-25 20:55:08 +0530

    Refactor User class with Lombok annotations

--- src/main/java/com/javaproject/java_project/model/User.java
+++ src/main/java/com/javaproject/java_project/model/User.java
@@ -1,13 +1,18 @@
 package com.javaproject.java_project.model;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.List;
 
 @Data
-public class User {
-    private int ID;
-    private String userName;
+@Getter
+@Setter
+public class User
+{
+    private int id;
+    private String username;
     private String passwordHash;
     private int level;
     private int xp;
