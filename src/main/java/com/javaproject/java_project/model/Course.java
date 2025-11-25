09a3b3a5853dc8f2d215f841846d83c188f887a5
sricharanand <58458878+sricharanand@users.noreply.github.com>
2025-11-25 20:55:29 +0530

    Refactor Course class with Lombok annotations

--- src/main/java/com/javaproject/java_project/model/Course.java
+++ src/main/java/com/javaproject/java_project/model/Course.java
@@ -1,12 +1,17 @@
 package com.javaproject.java_project.model;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import java.util.List;
 
+@Getter
+@Setter
 @Data
-public class Course{
-    private int courseID;
+public class Course
+{
+    private int courseId;
     private String courseName;
-    private int userID; // owner
+    private int userId; // owner
     private List<Integer> tasks; // stored by IDs
 }
