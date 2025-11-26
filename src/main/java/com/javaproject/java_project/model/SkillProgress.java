0db920601e30718c38b26c3b2d2cb4ff36a60005
3sha <thrisshaarcot29@gmail.com>
2025-11-26 12:45:40 +0530

    Model for SkillProgress

--- src/main/java/com/javaproject/java_project/model/SkillProgress.java
+++ src/main/java/com/javaproject/java_project/model/SkillProgress.java
@@ -1,4 +1,24 @@
 package com.javaproject.java_project.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class SkillProgress {
+
+    @Builder.Default
+    private int level = 1;
+
+    @Builder.Default
+    private int xp = 0;
+
+    private String courseName; // Name of the course this progress tracks
+
+    @Builder.Default
+    private String levelName = "Novice Scholar";
 }
